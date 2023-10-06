e22ab3b2479a0b53fbb2dd0644fd76866e19a509
bohdanalu <b_dana@ukr.net>
2023-10-06 13:20:23 +0300

    add new image, add component ArrowIcon, imolement icon in other components

--- components/LanguageSwitcher.jsx
+++ components/LanguageSwitcher.jsx
@@ -1,6 +1,7 @@
 "use client";
 import { useState } from "react";
 import Link from "next/link";
+import Image from "next/image";
 
 const LanguageSwitcher = () => {
   const [isDropdownVisible, setIsDropdownVisible] = useState(false);
@@ -14,6 +15,14 @@ const LanguageSwitcher = () => {
       onClick={toggleDropdown}
     >
       <span className="title-switcher relative cursor-pointer">Мова</span>
+      <Image
+        src="/icons/arrow.svg"
+        width="20"
+        height="20"
+        className={`inline-block ml-1 transition-transform duration-300 ${
+          isDropdownVisible ? "transform rotate-180" : ""
+        }`}
+      />
       <ul
         className={`dropdown w-full absolute top-7 left-0 flex flex-col gap-[10px] border rounded border-solid border-gray p-4 transition-opacity duration-300 
        ${isDropdownVisible ? "visible opacity-100" : "invisible opacity-0"} `}
--- components/ThemeSwitcher.jsx
+++ components/ThemeSwitcher.jsx
@@ -1,5 +1,6 @@
 "use client";
 import { useState } from "react";
+import Image from "next/image";
 
 const ThemeSwitcher = () => {
   const [isDropdownVisible, setIsDropdownVisible] = useState(false);
@@ -13,6 +14,14 @@ const ThemeSwitcher = () => {
       onClick={toggleDropdown}
     >
       <span className="title-switcher relative cursor-pointer">Тема</span>
+      <Image
+        src="/icons/arrow.svg"
+        width="20"
+        height="20"
+        className={`inline-block ml-1 transition-transform duration-300 ${
+          isDropdownVisible ? "transform rotate-180" : ""
+        }`}
+      />
       <ul
         className={`dropdown w-full absolute top-7 left-0 flex flex-col gap-[10px] border rounded border-solid border-gray p-4 transition-opacity duration-300 
          ${isDropdownVisible ? "visible opacity-100" : "invisible opacity-0"} `}
