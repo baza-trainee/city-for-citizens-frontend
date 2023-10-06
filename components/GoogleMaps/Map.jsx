271f614a156e226b4eb08c4e8e488b6e3acf47da
dairinka <interest4me@gmail.com>
2023-10-06 21:05:52 +0300

    feat: add FilteredMap component; add markers on map

--- components/GoogleMaps/Map.jsx
+++ components/GoogleMaps/Map.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { GoogleMap, MarkerF, useJsApiLoader } from "@react-google-maps/api";
 
 const containerStyle = {
@@ -13,15 +13,38 @@ const center = {
   lng: 31.387372519412626,
 };
 
-const Map = () => {
+const Map = ({citiesData}) => {
+  console.log("citiesData in Map:", citiesData);
   const { isLoaded } = useJsApiLoader({
     id: "google-map-script",
     googleMapsApiKey: "",
   });
+  const [markers, setMarkers] = useState([]);
+  
+  useEffect(() => {
+    if (isLoaded) {
+      const markers = citiesData.map((cityObj) => {
+        const cityName = Object.keys(cityObj)[0];
+        const [lat, lng] = cityObj[cityName];
+        return (
+              <MarkerF 
+                key={cityName}
+                position={{
+                  lat,
+                  lng,
+                }}
+              />
+            );
+      });
+      setMarkers(markers);
+    }
+  }, [isLoaded, citiesData]);
 
   return isLoaded ? (
     <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={6.5}>
-      <MarkerF
+     {markers}    
+    
+      {/* <MarkerF
         position={{
           lat: 49.047614511330444,
           lng: 31.387372519412626,
@@ -32,7 +55,7 @@ const Map = () => {
           lat: 50.443310,
           lng: 30.518350,
         }}
-      />
+      /> */}
     </GoogleMap>
   ) : (
     <></>
