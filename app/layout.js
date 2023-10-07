b0bac78af947dec31d83fabe99e877692862955f
vasylkivt <vasylkiv.t@gmail.com>
2023-10-07 15:55:37 +0300

    feat: add font-family NewMexika; add font-family Mak

--- app/layout.js
+++ app/layout.js
@@ -1,20 +1,14 @@
-import localFont from 'next/font/local'
-import './globals.css'
-
-const myFont = localFont({
-  src: '../public/fonts/NewMexika.ttf',
-  display: 'swap',
-})
+import './globals.css';
 
 export const metadata = {
   title: 'Мiсто для мiстян',
   description: 'Мiсто для мiстян',
-}
+};
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className={myFont.className}>
-      <body className='bg-gray/5'>{children}</body>
+    <html lang="en">
+      <body className="bg-gray/5">{children}</body>
     </html>
-  )
+  );
 }
