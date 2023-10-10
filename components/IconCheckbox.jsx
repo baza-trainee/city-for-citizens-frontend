cc6cb3effb728f214dca3cffbbc0ed34996fc99f
vasylkivt <vasylkiv.t@gmail.com>
2023-10-10 20:09:03 +0300

    feat: add svg-component IconCheckbox

---
+++ components/IconCheckbox.jsx
@@ -0,0 +1,14 @@
+const IconCheckbox = props => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" {...props}>
+      <path
+        fill="none"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M29.334 9.333 16.001 22.666l-.833-.833M9.334 16v0zM2.666 16l6.667 6.667L22.666 9.334"
+      />
+    </svg>
+  );
+};
+export default IconCheckbox;
