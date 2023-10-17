5b60afaa94c695dc45a1bc765efa4ccfe645c073
vasylkivt <vasylkiv.t@gmail.com>
2023-10-17 18:56:33 +0300

    feat: added setting filters in url; added useQueryParam hook;

--- components/GoogleMaps/DatePicker/DatePicker.jsx
+++ components/GoogleMaps/DatePicker/DatePicker.jsx
@@ -6,9 +6,11 @@ import { uk, en } from 'date-fns/locale';
 
 import IconSelectArrow from '../../icons/IconSelectArrow';
 import FilterInputWrapper from '../FilterInputWrapper';
-import { customComponents } from './customComponents';
-
 import { formatDate, generateDateRange } from '@/helpers';
+import { customStylesDatePicker } from './customStylesDatePicker';
+import { customComponents } from './customComponents';
+import { useQueryParam } from '@/hooks';
+
 import { activeDates } from '../temporaryData/temporaryActiveDate';
 
 // const eng = {
@@ -21,18 +23,33 @@ const ukr = {
   inputText: 'Час',
 };
 
-export const DatePicker = ({ setDate }) => {
+export const DatePicker = () => {
   const [range, setRange] = useState({});
-  const [dateToFilter, setDateToFilter] = useState([]);
   const [inputText, setInputText] = useState([]);
+  const [dateToFilter, setDateToFilter] = useQueryParam('date');
 
   useEffect(() => {
-    setDate(dateToFilter);
-  }, [dateToFilter, setDate]);
+    if (dateToFilter.length === 0) {
+      return;
+    }
+    if (range?.from) {
+      return;
+    }
+    if (range === undefined) {
+      setDateToFilter([]);
+      setInputText([]);
+      return;
+    }
+    setRange({
+      from: new Date(dateToFilter[0]),
+      to:
+        dateToFilter[0] === dateToFilter.at(-1)
+          ? undefined
+          : new Date(dateToFilter.at(-1)),
+    });
+  }, [dateToFilter, range, setDateToFilter]);
 
   useEffect(() => {
-    setDateToFilter([]);
-    setInputText([]);
     if (range?.from) {
       const from = formatDate(range.from);
       const to = range.to ? formatDate(range.to) : null;
@@ -49,36 +66,9 @@ export const DatePicker = ({ setDate }) => {
         setInputText([from, to]);
       }
     }
-  }, [range]);
+  }, [range, setDateToFilter]);
 
   const isDateActive = date => activeDates.includes(formatDate(date));
-
-  const customStylesDatePicker = {
-    root: 'text-gray/80 dark:text-gray/5 p-[16px] ', // calendar wrap
-
-    caption:
-      'relative mb-[20px] tablet:mb-[10px] h-[24px] flex justify-center items-end', // mouth, nav button
-    caption_label:
-      'text-center capitalize text-[14px] leading-[1.5] -tracking-[0.154px]  text-gray/100 dark:text-gray/5 ', //mouth title
-    nav: 'absolute top-0 w-full flex justify-between', // nav button wrap
-    nav_button: 'w-auto', // nav button
-
-    table:
-      'w-full flex flex-col gap-[20px] tablet:gap-[15px] desktop:gap-[20px]', // wrap - day week name, day mouth number
-    tbody: 'flex flex-col gap-[20px] tablet:gap-[15px] desktop:gap-[20px]', // wrap day mouth number
-    head_row: 'flex justify-between', // row with day week name
-    row: 'flex justify-between ', // rows with day mouth number
-
-    head_cell:
-      'w-[25px] desktop:w-[20px] p-0 text-[12px] font-normal select-none capitalize ', // wrap/item day week name
-    cell: 'p-0 text-center', // wrap/item day mouth number
-    button:
-      'w-[25px] desktop:w-[20px] block hover:text-primary/100 hover:dark:text-primary/80 text-[12px] select-none leading-[1.5] -tracking-[0.132px]', // button day mouth number
-
-    day_selected: 'text-primary/100 dark:text-primary/80 ', // selected day mouth number
-    day_disabled:
-      'opacity-100 !text-gray/20  dark:!text-gray/50 pointer-events-none', // disabled day mouth number
-  };
 
   return (
     <div className="tablet:w-[264px] desktop:w-[305px]">
