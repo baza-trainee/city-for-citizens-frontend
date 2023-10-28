ce854ae65e28f71285bfe5a7cb299437cc89be0a
bohdanalu <b_dana@ukr.net>
2023-10-28 21:21:45 +0300

    fix: add and change styles to footer content

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,13 +1,16 @@
-import Link from "next/link";
+import Link from 'next/link';
 
 const Footer = () => {
   return (
-    <footer className="pt-[72px] pb-[72px] bg-gray/100 text-gray/5 dark:bg-gray/100 dark:text-gray/5">
-      <div className="container flex gap-[49px] ">
-        <Link className="mr-[54px]" href={"/"}>
-          Місто для містян
-        </Link>
-        <p>Baza Trainee Ukraine 2023 &copy; Всі права захищені</p>
+    <footer className="bg-gray/100 py-[20px] text-gray/5 dark:bg-gray/100 dark:text-gray/5 tablet:py-[72px]">
+      <div
+        className="container flex flex-wrap items-center justify-center gap-[8px] 
+      tracking-[-0.176px] tablet:gap-[49px]"
+      >
+        <Link href={'/'}>Місто для містян</Link>
+        <p className="text-sm tablet:text-base">
+          Baza Trainee Ukraine 2023&copy; Всі права захищені
+        </p>
       </div>
     </footer>
   );
