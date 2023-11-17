1631d857205c87129707bcb70a3b520f7a1c6490
vasylkivt <vasylkiv.t@gmail.com>
2023-11-17 08:27:57 +0200

    feat: add correct BASE_URL;

--- helpers/constants.js
+++ helpers/constants.js
@@ -1,5 +1,5 @@
 const LOCALE = { en: 'en_US', uk: 'uk_UA' };
 
-const BASE_URL = 'http://45.94.157.117:4000/api';
+const BASE_URL = 'http://185-237-14-12.cloud-xip.com:8082/api';
 
 export { LOCALE, BASE_URL };
