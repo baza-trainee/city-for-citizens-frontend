17b52473b22f5a784a9723d1b61e1661e87a276e
vasylkivt <vasylkiv.t@gmail.com>
2023-12-10 21:42:07 +0200

    fix : add a fixed color marker when opening a modal window;

--- components/icons/IconMapMarker.jsx
+++ components/icons/IconMapMarker.jsx
@@ -1,14 +1,14 @@
-const IconMapMarker = ({ isHovered, theme }) => {
+const IconMapMarker = ({ isHovered, theme, isActive }) => {
   const initialSvgMarker = {
     path: 'M12 22s7.5-6 7.5-12.5a7.5 7.5 0 0 0-15 0C4.5 16 12 22 12 22Z M12 12.5a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z',
     fillColor: 'none',
     strokeWeight: 2,
     strokeColor:
       theme === 'dark'
-        ? isHovered
+        ? isHovered || isActive
           ? '#6589E3'
           : '#F6F6F6'
-        : isHovered
+        : isHovered || isActive
         ? '#0D3BDD'
         : '#121923',
     scale: 1.5,
