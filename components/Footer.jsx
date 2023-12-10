6a1b562c4d77bcaa26bdced3fbb99b537bdb554d
vasylkivt <vasylkiv.t@gmail.com>
2023-12-10 21:52:34 +0200

    fix: add correct style to Footer;

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,16 +1,18 @@
+import { Link } from '@/navigation';
 import { useTranslations } from 'next-intl';
-import Link from 'next/link';
 
 const Footer = () => {
   const t = useTranslations('Footer');
 
   return (
-    <footer className="bg-gray/100 py-[20px] text-gray/5 dark:bg-gray/100 dark:text-gray/5 tablet:py-[72px]">
+    <footer className="bg-gray/100 py-[20px] text-gray/5 dark:bg-gray/100 tablet:py-[72px]">
       <div
         className="container flex flex-wrap items-center justify-center gap-[8px] 
       tracking-[-0.176px] tablet:gap-[49px]"
       >
-        <Link href={'/'}>{t('logotype')}</Link>
+        <Link className="tablet:text-[20px]" href={'/'}>
+          Місто для містян
+        </Link>
         <p className="text-sm tablet:text-base">
           {`${t('company')} ${t('year')}  ${t('copyrightText')}`}
         </p>
