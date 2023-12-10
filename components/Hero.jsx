40e8a95b214842fc1cf45544abc4b08a25da0650
vasylkivt <vasylkiv.t@gmail.com>
2023-12-10 19:17:59 +0200

    fix: add correct styles to Hero section;

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -4,27 +4,27 @@ const Hero = () => {
   const t = useTranslations('Hero');
 
   return (
-    <>
-      <section
-        className="relative z-30 bg-gray/5 pt-[18px] text-gray/100
-       dark:bg-gray/100 dark:text-gray/5 desktop:z-0 desktop:pt-[52px]"
-      >
-        <div className="container max-tablet:max-w-full">
-          <h1
-            className="mb-[12px] text-center font-heading text-[48px] font-light leading-[1.2] -tracking-[0.528px]
-          max-tablet:text-[34px]"
-          >
-            {t('title')}
-          </h1>
-          <h2
-            className="mb-[32px] text-center text-[24px]  leading-[1.5] -tracking-[0.264px]
-                  max-tablet:text-[20px]"
-          >
-            {t('subtitle')}
-          </h2>
-        </div>
-      </section>
-    </>
+    <section
+      className="relative z-30 bg-gray/5 pb-[32px] pt-[22px] text-gray/100 
+      dark:bg-gray/100 dark:text-gray/5 
+      tablet:pb-[38px] tablet:pt-[16px] 
+      desktop:z-0 desktop:pb-[44px] desktop:pt-[32px]"
+    >
+      <div className="container max-tablet:max-w-full">
+        <h1
+          className="mb-[12px] text-center font-heading text-[34px] font-light leading-[1.2] -tracking-[0.528px]
+          desktop:text-[64px]"
+        >
+          {t('title')}
+        </h1>
+        <h2
+          className=" text-center text-[20px]  leading-[1.5] -tracking-[0.264px]
+                  desktop:text-[24px]"
+        >
+          {t('subtitle')}
+        </h2>
+      </div>
+    </section>
   );
 };
 export default Hero;
