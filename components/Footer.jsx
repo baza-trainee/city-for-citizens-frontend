265b92770432bc3e41095e4b5031f2d34f4a183f
bohdanalu <b_dana@ukr.net>
2023-12-18 21:51:35 +0200

    fix: add wrap in footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,11 +1,13 @@
 import { Link } from '@/navigation';
 import { useTranslations } from 'next-intl';
 
+const wrap
+
 const FooterLink = ({ href, label }) => {
   return (
     <a
       href={`${href}#toolbar=0`}
-      // onContextMenu={e => e.preventDefault()}
+      onContextMenu={e => e.preventDefault()}
       target="_blank"
       rel="noopener noreferrer"
       className="underline underline-offset-2 hover:text-gray/30"
@@ -20,11 +22,12 @@ const Footer = () => {
 
   return (
     <footer className="bg-gray/100 py-[20px] text-gray/5 dark:bg-gray/100 tablet:py-[72px]">
-      <div className="flex flex-col flex-wrap items-center gap-4 tracking-[-0.176px] tablet:gap-6">
+      <div className="flex flex-col items-center gap-4 tracking-[-0.176px] tablet:gap-6">
         <Link className="tablet:text-[20px]" href={'/'}>
           Місто для містян
         </Link>
-        <div className="flex flex-col items-center justify-center gap-1 tablet:flex-row">
+
+        <div className="flex flex-col items-center justify-center gap-2 tablet:flex-row">
           <FooterLink
             href="/consent/privacy-policy.pdf"
             label={t('labelPrivacyPolicy')}
@@ -35,19 +38,19 @@ const Footer = () => {
             label={t('labelPersonalData')}
           />
         </div>
-        <p className="text-sm tablet:text-base">
-          {`${t('company')} ${t('year')}  ${t('copyrightText')}    |   ${t(
-            'hosted'
-          )}`}{' '}
+
+        <div className="flex flex-col items-center justify-center gap-1 text-sm tablet:flex-row tablet:text-base">
+          <p>{`${t('company')} ${t('year')} ${t('copyrightText')} `}</p>
+          <span className="hidden tablet:inline-block">|</span>
           <a
             className="hover:text-gray/30"
             href="https://deltahost.com"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Delta Host
+            {t('hosted')} Delta Host
           </a>
-        </p>
+        </div>
       </div>
     </footer>
   );
