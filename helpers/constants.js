cfcd0c45c7cfb74bbc51edb09281155c04504c4e
vasylkivt <vasylkiv.t@gmail.com>
2023-12-20 13:13:06 +0200

    add correct path to image;

--- helpers/constants.js
+++ helpers/constants.js
@@ -11,8 +11,8 @@ const NAVIGATION = {
 };
 
 // const BASE_URL = 'http://45.94.157.117:53431/api';
-// const IMAGE_BASE_URL = 'http://45.94.157.117:53431/';
+// const IMAGE_BASE_URL = 'http://45.94.157.117:53431/images/events/';
 const BASE_URL = 'https://city-backend-45go.onrender.com/api';
-const IMAGE_BASE_URL = 'https://city-backend-45go.onrender.com';
+const IMAGE_BASE_URL = 'https://city-backend-45go.onrender.com/images/events/';
 
 export { LOCALE, BASE_URL, IMAGE_BASE_URL, NAVIGATION };
