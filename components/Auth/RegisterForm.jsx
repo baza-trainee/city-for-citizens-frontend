d1654543504de1497495ea6efeea505f89f8baae
Bohdana Lutska <87516048+bohdanalu@users.noreply.github.com>
2024-01-02 18:46:12 +0200

    add styles to Register Form (#53)
    * Feature: add styles to the Register Form

--- components/Auth/RegisterForm.jsx
+++ components/Auth/RegisterForm.jsx
@@ -5,6 +5,7 @@ import { Link, useRouter } from '@/navigation';
 import { register } from '@/services/authAPI';
 import { useState } from 'react';
 import { publicRoute } from '../publicRoute';
+import Input from './Input';
 
 const RegisterForm = () => {
   const [formData, setFormData] = useState({
@@ -27,6 +28,7 @@ const RegisterForm = () => {
 
   const handleSubmit = async e => {
     e.preventDefault();
+
     try {
       const { accessToken } = await register(formData);
 
@@ -45,40 +47,42 @@ const RegisterForm = () => {
     <div className="container">
       <form
         onSubmit={handleSubmit}
-        className="flex flex-col items-center gap-[30px]"
+        className="mx-auto flex max-w-[394px] flex-col items-center gap-[30px] py-4"
       >
-        <label>
-          <span>Name</span>
-          <input value={name} onChange={handleChange} name="name" type="text" />
-        </label>
-        <label>
-          <span>Email</span>
-          <input
-            value={email}
-            onChange={handleChange}
-            name="email"
-            type="text"
-          />
-        </label>
-        <label>
-          <span>Password</span>
-          <input
-            value={password}
-            onChange={handleChange}
-            name="password"
-            type="text"
-          />
-        </label>
+        <Input
+          label="Name"
+          placeholder="Enter name"
+          value={name}
+          onChange={handleChange}
+          name="name"
+          type="text"
+        />
+        <Input
+          label="Email"
+          placeholder="Enter email"
+          value={email}
+          onChange={handleChange}
+          name="email"
+          type="text"
+        />
+        <Input
+          label="Password"
+          placeholder="Enter password"
+          value={password}
+          onChange={handleChange}
+          name="password"
+          type="text"
+        />
 
         {error && <p>{error}</p>}
 
         <p>
           Already have an account?{' '}
-          <Link href="/login">
+          <Link className="ml-2 hover:text-gray/30" href="/login">
             <u>Log in.</u>
           </Link>
         </p>
-        <button className="mx-auto my-0 block rounded-[10px] bg-primary/80 px-[40px] py-[10px]">
+        <button className="mx-auto my-0 block w-full rounded-lg bg-primary/100 p-2.5 px-[40px] py-[10px] text-gray/0 hover:bg-primary/80 dark:border-gray/5 dark:bg-gray/5 dark:text-gray/100 dark:hover:border-gray/10 dark:hover:bg-gray/10">
           Sing Up
         </button>
       </form>
