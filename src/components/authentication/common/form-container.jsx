69775dd782ff8614c6ddafff29104bca6f7c3136
Bohdana Lutska <87516048+bohdanalu@users.noreply.github.com>
2024-02-08 21:36:57 +0200

    add message about password mismatchs (#82)

--- src/components/authentication/common/form-container.jsx
+++ src/components/authentication/common/form-container.jsx
@@ -4,7 +4,7 @@ import SucceessMessage from './success-message';
 export default function FormContainer({ children, error, message }) {
   return (
     <div
-      className={`absolute inset-x-0 flex max-h-[120vh] min-h-screen ${error ? 'items-center pb-[7%]' : 'items-center pt-[2%]'} justify-center overflow-y-auto bg-admin-dark bg-opacity-50 `}
+      className={`absolute inset-x-0 flex max-h-[120vh] min-h-screen ${error ? 'items-center pb-[7%]' : 'items-center pt-[3%]'} justify-center overflow-y-auto bg-admin-dark bg-opacity-50 `}
     >
       <div className="flex w-[460px] flex-col gap-[30px]">
         {error && <ErrorMessage error={error} />}
@@ -12,7 +12,7 @@ export default function FormContainer({ children, error, message }) {
           role="dialog"
           aria-modal="true"
           aria-labelledby="modal-headline"
-          className="bg-auth-light_2 relative  flex flex-col gap-8 rounded-lg p-6 text-auth-dark"
+          className="relative flex  flex-col gap-8 rounded-lg bg-auth-light_2 p-6 text-auth-dark"
         >
           {children}
         </div>
