c3386e8b898369b89d9a38a51044cd984f7b740c
Iryna Yehorova <interest4me@gmail.com>
2024-02-08 22:20:37 +0200

    fix: correct button styles

--- src/components/admin-panel/event-list/event-list-all/add-event-button.jsx
+++ src/components/admin-panel/event-list/event-list-all/add-event-button.jsx
@@ -4,7 +4,7 @@ export default function AddEventButton() {
   return (
     <Link
       className="group block self-center rounded-lg bg-admin-dark px-[1.88rem] py-2.5
-        text-center text-xl font-bold text-admin-light_3 transition duration-200 hover:bg-admin-darkgray hover:text-[#ffffff]"
+        text-center text-xl font-bold text-admin-light_3 transition duration-200 hover:bg-light-button-hover hover:text-[#ffffff] active:bg-admin-darkblue"
       href={'/admin/event'}
     >
       <button type="button" title="Додати подію">
