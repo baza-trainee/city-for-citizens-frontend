66e662af70618bc30a35cbd091c3f72e256789ed
vasylkivt <vasylkiv.t@gmail.com>
2024-02-10 00:13:40 +0200

    fix: add a correct working to-edit-event button;

--- src/app/[locale]/admin/page.jsx
+++ src/app/[locale]/admin/page.jsx
@@ -1,8 +1,5 @@
 import EventList from '@/components/admin-panel/event-list/event-list-all/event-list';
-import { unstable_setRequestLocale } from 'next-intl/server';
 
-export default function Page({ params: { locale } }) {
-  unstable_setRequestLocale(locale);
-
+export default function Page() {
   return <EventList />;
 }
