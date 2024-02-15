99504d58367142e31921218bfb4eeba45a6da14e
Bohdana Lutska <87516048+bohdanalu@users.noreply.github.com>
2024-02-15 22:37:13 +0200

    fix: change hover styles for link (#96)

--- src/components/authentication/common/go-back-link.jsx
+++ src/components/authentication/common/go-back-link.jsx
@@ -4,7 +4,7 @@ export default function GoBackLink() {
   return (
     <Link
       href="/login"
-      className="flex w-[182px] justify-center rounded  border border-auth-dark bg-white py-[10px] text-xl font-bold  text-auth-dark hover:opacity-70"
+      className="flex w-[182px] justify-center rounded border  border-auth-dark bg-white py-[10px] text-xl font-bold text-auth-dark hover:bg-admin-button-hover_outlined  active:bg-admin-button-active_outlined"
     >
       Повернутися
     </Link>
