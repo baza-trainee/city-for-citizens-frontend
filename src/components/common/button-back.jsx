455f95e5d8aafbcc2fefb87e31de52acf5341fd7
vasylkivt <vasylkiv.t@gmail.com>
2024-02-20 12:46:03 +0200

    feat: add hover to event form buttons;

--- src/components/common/button-back.jsx
+++ src/components/common/button-back.jsx
@@ -3,20 +3,21 @@
 import BackIcon from '@/assets/icons/common/back-icon.svg';
 
 import { useRouter } from 'next/navigation';
+import { Button } from '@/components/common';
 
 export default function ButtonBack() {
   const router = useRouter();
 
   return (
-    <button
-      className="button-common flex h-[51px] w-[185px] items-center justify-start gap-[7px]  pl-8 text-admin-dark"
-      type="button"
+    <Button
+      variant={'outlined'}
       onClick={() => {
         router.back();
       }}
+      className={'h-[51px] w-[185px] gap-[10px] pr-[15px]'}
     >
-      <BackIcon className={'h-7 w-7 scale-y-[-1] text-admin-dark'} />
-      Назад
-    </button>
+      <BackIcon className={'h-7 w-7 scale-y-[-1]'} />
+      <p>Назад</p>
+    </Button>
   );
 }
