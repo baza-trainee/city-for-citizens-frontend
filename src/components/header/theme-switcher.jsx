3db087745ede5518ecac4a622737777a8694345d
bohdanalu <b_dana@ukr.net>
2024-02-21 11:18:19 +0200

    refactor: rewrite theme swither for better using

--- src/components/header/theme-switcher.jsx
+++ src/components/header/theme-switcher.jsx
@@ -5,23 +5,25 @@ import IconMoon from '@/assets/icons/theme-switchers/icon-moon.svg';
 
 export default function ThemeSwitcher() {
   const { theme, setTheme } = useTheme();
+  const toggleTheme = () => {
+    setTheme(theme === 'light' ? 'dark' : 'light');
+  };
 
   return (
-    <div className="mx-2 flex h-4 w-8 items-center justify-between rounded-full bg-light-button-switch p-[1px] dark:bg-dark-button-switch">
-      <button
+    <button
+      type="button"
+      onClick={toggleTheme}
+      className="mx-2 flex h-4 w-8 items-center justify-between rounded-full bg-light-button-switch p-[1px] dark:bg-dark-button-switch"
+    >
+      <div
         className="flex h-[13px] w-[13px] items-center justify-center rounded-full bg-dark-button-switch"
         type="button"
-        onClick={() => setTheme('light')}
       >
         {theme === 'light' ? <IconSun width={8} height={8} /> : ''}
-      </button>
-      <button
-        className="flex h-[13px] w-[13px] items-center justify-center rounded-full bg-light-button-switch"
-        type="button"
-        onClick={() => setTheme('dark')}
-      >
+      </div>
+      <div className="flex h-[13px] w-[13px] items-center justify-center rounded-full bg-light-button-switch">
         {theme === 'dark' ? <IconMoon width={8} height={8} /> : ''}
-      </button>
-    </div>
+      </div>
+    </button>
   );
 }
