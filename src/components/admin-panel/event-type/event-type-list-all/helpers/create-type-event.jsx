c1cfac28e7fae9de2fe821c0e1f6f23242343048
Iryna Yehorova <104695300+dairinka@users.noreply.github.com>
2024-02-22 10:31:26 +0200

    fix: correct type event query (#109)

--- src/components/admin-panel/event-type/event-type-list-all/helpers/create-type-event.jsx
+++ src/components/admin-panel/event-type/event-type-list-all/helpers/create-type-event.jsx
@@ -1,8 +1,9 @@
 import { useForm } from 'react-hook-form';
-import { getValidationSchemaCreate } from '@/components/admin-panel/event-type/event-type-list-all/helpers/validationSchema';
+import { getValidationSchema } from '@/components/admin-panel/event-type/event-type-list-all/helpers/validationSchema';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { useCreateTypeEventMutation } from '@/redux/api/typesEventApi';
 import { BasicModalWindows } from '@/components/common';
+import CommonModalForm from './common-modal-form';
 
 export default function CreateTypeEvent({ close, success, error }) {
   const [addTypeEvent, { isLoading: isLoadingAddTypeEvent }] =
@@ -11,23 +12,26 @@ export default function CreateTypeEvent({ close, success, error }) {
   const {
     register,
     handleSubmit,
-    formState: { errors },
+    formState: { errors, isValid },
   } = useForm({
     mode: 'all',
     defaultValues: {
       ukTypeName: '',
       enTypeName: '',
     },
-    resolver: yupResolver(getValidationSchemaCreate()),
+    resolver: yupResolver(getValidationSchema()),
   });
 
   async function onSubmit(data) {
     close();
     try {
-      const ukFormData = { locale: 'uk_UA', eventType: data.ukTypeName };
-      const enFormData = { locale: 'en_US', eventType: data.enTypeName };
-      await addTypeEvent(ukFormData).unwrap();
-      await addTypeEvent(enFormData).unwrap();
+      const formData = {
+        eventTypeUkr: data.ukTypeName,
+        eventTypeEng: data.enTypeName,
+      };
+
+      await addTypeEvent(formData).unwrap();
+
       success();
     } catch (err) {
       error();
@@ -41,55 +45,13 @@ export default function CreateTypeEvent({ close, success, error }) {
         onSubmit={handleSubmit(onSubmit)}
         className="flex flex-col items-center gap-y-[25px]"
       >
-        <div className="-mt-1 flex flex-col gap-y-8">
-          <div className="relative flex flex-col">
-            <div className="mx-4 flex h-12 w-[407px] items-center rounded border border-admin-darkgray focus-within:border-admin-dark">
-              <input
-                {...register('ukTypeName')}
-                type="text"
-                placeholder="Введіть новий тип події українською мовою"
-                className="w-full pl-[7px] text-admin-dark outline-none"
-              />
-            </div>
-            {errors.ukTypeName && (
-              <div className="absolute -bottom-6 pl-[15px] text-admin-modal-error">
-                {errors.ukTypeName.message}
-              </div>
-            )}
-          </div>
-          <div className="relative flex flex-col">
-            <div className="mx-4 flex h-12 w-[407px] items-center rounded border border-admin-darkgray focus-within:border-admin-dark">
-              <input
-                {...register('enTypeName')}
-                type="text"
-                placeholder="Введіть новий тип події англійською мовою"
-                className="w-full pl-[7px] text-admin-dark outline-none "
-              />
-            </div>
-            {errors.enTypeName && (
-              <div className="absolute -bottom-6 pl-[15px] text-admin-modal-error">
-                {errors.enTypeName.message}
-              </div>
-            )}
-          </div>
-        </div>
-        <div className="mb-[25px] mt-2  flex gap-[10px]">
-          <button
-            disabled={isLoadingAddTypeEvent}
-            className="button-close-hover w-[198px] pb-3 pt-2"
-            onClick={close}
-            type="button"
-          >
-            Скасувати
-          </button>
-          <button
-            disabled={isLoadingAddTypeEvent}
-            className="button-confirm-hover w-[198px] pb-3 pt-2"
-            type="submit"
-          >
-            Додати
-          </button>
-        </div>
+        <CommonModalForm
+          register={register}
+          errors={errors}
+          isValid={isValid}
+          isLoading={isLoadingAddTypeEvent}
+          nameButton="Додати"
+        />
       </form>
     </BasicModalWindows>
   );
--- src/components/admin-panel/event-type/event-type-list-all/helpers/edit-type-event.jsx
+++ src/components/admin-panel/event-type/event-type-list-all/helpers/edit-type-event.jsx
@@ -1,8 +1,9 @@
 import { useForm } from 'react-hook-form';
-import { getValidationSchemaEdit } from '@/components/admin-panel/event-type/event-type-list-all/helpers/validationSchema';
+import { getValidationSchema } from '@/components/admin-panel/event-type/event-type-list-all/helpers/validationSchema';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { useUpdateTypesEventMutation } from '@/redux/api/typesEventApi';
 import { BasicModalWindows } from '@/components/common';
+import CommonModalForm from './common-modal-form';
 
 export default function EditTypeEvent({ typeEvent, close, success, error }) {
   const [editTypeEvent, { isLoading: isLoadingEditTypeEvent }] =
@@ -11,22 +12,26 @@ export default function EditTypeEvent({ typeEvent, close, success, error }) {
   const {
     register,
     handleSubmit,
-    formState: { errors, isDirty, isValid },
+    formState: { errors, isValid },
   } = useForm({
     mode: 'all',
     defaultValues: {
-      typeName: typeEvent.eventType,
+      ukTypeName: typeEvent[0].eventType,
+      enTypeName: typeEvent[1].eventType,
     },
-    resolver: yupResolver(getValidationSchemaEdit(typeEvent.locale)),
+    resolver: yupResolver(getValidationSchema()),
   });
 
   async function onSubmit(data) {
     close();
     try {
-      const formData = { locale: typeEvent.locale, eventType: data.typeName };
+      const formData = {
+        eventTypeUkr: data.ukTypeName,
+        eventTypeEng: data.enTypeName,
+      };
       await editTypeEvent({
         body: formData,
-        typeEventId: typeEvent.id,
+        typeEventId: typeEvent[0].idIdentifier,
       }).unwrap();
       success();
     } catch (err) {
@@ -40,39 +45,13 @@ export default function EditTypeEvent({ typeEvent, close, success, error }) {
         onSubmit={handleSubmit(onSubmit)}
         className="flex flex-col items-center gap-y-[25px]"
       >
-        <div className="relative flex flex-col">
-          <div className="mx-4 flex h-12 w-[407px] items-center rounded border border-admin-darkgray focus-within:border-admin-dark">
-            <input
-              {...register('typeName')}
-              type="text"
-              placeholder={`Введіть тип події ${typeEvent.locale === 'uk_UA' ? 'українською' : 'англійською'} мовою`}
-              className="w-full pl-[7px] text-admin-dark outline-none"
-            />
-          </div>
-          {errors.typeName && (
-            <div className="absolute -bottom-6  pl-[15px] text-admin-modal-error">
-              {errors.typeName.message}
-            </div>
-          )}
-        </div>
-
-        <div className="mb-[25px] mt-2  flex gap-[10px]">
-          <button
-            disabled={isLoadingEditTypeEvent}
-            className="button-close-hover w-[198px] pb-3 pt-2"
-            onClick={close}
-            type="button"
-          >
-            Скасувати
-          </button>
-          <button
-            disabled={isLoadingEditTypeEvent || !isDirty || !isValid}
-            className="button-confirm-hover w-[198px] pb-3 pt-2"
-            type="submit"
-          >
-            Редагувати
-          </button>
-        </div>
+        <CommonModalForm
+          register={register}
+          errors={errors}
+          isValid={isValid}
+          isLoading={isLoadingEditTypeEvent}
+          nameButton="Редагувати"
+        />
       </form>
     </BasicModalWindows>
   );
