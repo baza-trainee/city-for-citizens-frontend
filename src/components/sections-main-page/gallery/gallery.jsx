5c223a35d47afb4eaa4637a7f0f4bbda4a4b332b
Kateryna Verveda <katushka211@gmail.com>
2024-02-28 10:59:56 +0100

    update gallery component

--- src/components/sections-main-page/gallery/gallery.jsx
+++ src/components/sections-main-page/gallery/gallery.jsx
@@ -1,7 +1,14 @@
+'use client';
+import { Title } from '../common/title';
+import { ImageGallery } from './slider';
+
 export function Gallery() {
   return (
-    <section className="flex h-[1137px] items-center justify-center bg-black/30">
-      <div>Gallery</div>
+    <section className="flex items-center justify-center text-center">
+      <div className=" w-full bg-light-secondary px-4 dark:bg-dark-primary  tablet:w-[768px] tablet:px-0 laptop:w-[960px] desktop_xl:w-[1920px] desktop_xl:px-[30px]">
+        <Title text={'Галерея'} />
+        <ImageGallery />
+      </div>
     </section>
   );
 }
