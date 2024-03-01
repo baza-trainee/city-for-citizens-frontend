8a7a2b0185d756e3be371199859e244ac16146e9
Volodymyr Peretiatko <djbob2000@gmail.com>
2024-03-01 20:18:02 +0200

    feat: create section footer

---
+++ src/components/footer/footer-nav.jsx
@@ -0,0 +1,37 @@
+'use client';
+import { useTranslations } from 'next-intl';
+import { Link as ScrollLink } from 'react-scroll';
+
+export default function FooterNav() {
+  const t = useTranslations('MenuItems');
+
+  const menuItems = [
+    { id: 'map', label: t('map') },
+    { id: 'gallery', label: t('gallery') },
+    { id: 'partners', label: t('partners') },
+  ];
+
+  return (
+    <nav>
+      <ul className="flex flex-col gap-3 text-center font-roboto text-base font-normal leading-[1.4] text-light-main dark:text-dark-main desktop:mr-auto desktop:text-left">
+        {menuItems.map(item => (
+          <li
+            key={item.id}
+            className="cursor-pointer hover:text-light-accent hover:transition-all dark:hover:text-dark-accent"
+          >
+            <ScrollLink
+              to={item.id}
+              smooth={true}
+              duration={500}
+              spy={true}
+              exact="true"
+              offset={-80}
+            >
+              {item.label}
+            </ScrollLink>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+}
