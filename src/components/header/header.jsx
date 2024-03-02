8cca20a96647dac074f8dc44921ff95fef34e462
Volodymyr Peretiatko <djbob2000@gmail.com>
2024-03-02 14:13:46 +0200

    fix: header padding

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -5,7 +5,7 @@ import Logo from '../common/logo';
 export function Header() {
   return (
     <header className="fixed left-0  top-0 z-50  w-screen  bg-light-secondary dark:bg-dark-secondary">
-      <div className="container flex h-[80px]  max-w-full items-center justify-between">
+      <div className="container flex h-[80px]  max-w-[1440px] items-center justify-between desktop:px-[40px]">
         <Logo />
         <MenuNavigation />
         <Switchers />
