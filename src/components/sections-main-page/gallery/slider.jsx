034bb40e814aaa78dc24417d554a1038ff9fef4a
vasylkivt <vasylkiv.t@gmail.com>
2024-03-02 11:29:38 +0200

    fix: conflict styles

--- src/components/sections-main-page/gallery/slider.jsx
+++ src/components/sections-main-page/gallery/slider.jsx
@@ -42,8 +42,8 @@ export function ImageGallery() {
       breakpoints={{
         768: {
           navigation: {
-            prevEl: '.swiper-button-prev',
-            nextEl: '.swiper-button-next',
+            prevEl: '.gallery-swiper-swiper-button-prev',
+            nextEl: '.gallery-swiper-swiper-button-next',
           },
           grid: {
             rows: 2,
@@ -52,27 +52,27 @@ export function ImageGallery() {
         960: {
           spaceBetween: 30,
           navigation: {
-            prevEl: '.swiper-button-prev',
-            nextEl: '.swiper-button-next',
+            prevEl: '.gallery-swiper-swiper-button-prev',
+            nextEl: '.gallery-swiper-swiper-button-next',
           },
           grid: {
             rows: 2,
           },
         },
       }}
-      className="mySwiper relative"
+      className="gallery-swiper relative"
     >
-      <div className="swiper-button-prev dark:bg-dark-butt absolute  bottom-[53%] z-10 hidden h-12 w-12 rounded-[40px] bg-light-button-default text-center opacity-50 dark:bg-dark-button-default tablet:left-4 tablet:block laptop:left-10 desktop:left-[10px]">
+      <div className="gallery-swiper-swiper-button-prev dark:bg-dark-butt absolute  bottom-[53%] z-10 hidden h-12 w-12 rounded-[40px] bg-light-button-default text-center opacity-50 dark:bg-dark-button-default tablet:left-4 tablet:block laptop:left-10 desktop:left-[10px]">
         <ArrowLeftIcon className="absolute left-[40%] top-[40%] h-6 w-6 -translate-x-1/2 -translate-y-1/2 transform" />
       </div>
-      <div className="swiper-button-next absolute bottom-[53%] z-10 h-12 w-12 rounded-[40px] bg-light-button-default opacity-50 dark:bg-dark-button-default mobile:hidden  tablet:right-4 tablet:block laptop:right-10 desktop:right-[10px]">
+      <div className="gallery-swiper-swiper-button-next absolute bottom-[53%] z-10 h-12 w-12 rounded-[40px] bg-light-button-default opacity-50 dark:bg-dark-button-default mobile:hidden  tablet:right-4 tablet:block laptop:right-10 desktop:right-[10px]">
         <ArrowRightIcon className="absolute left-[40%] top-[40%] h-6 w-6 -translate-x-1/2 -translate-y-1/2 transform" />
       </div>
       {data?.map((item, index) => (
         <SwiperSlide
           key={index}
           style={{ width: getWidthByIndex(index), height: '300px' }}
-          className="group relative overflow-hidden rounded-lg"
+          className="gallery-swiper group relative overflow-hidden rounded-lg"
         >
           <Image
             src={item.url}
@@ -81,7 +81,7 @@ export function ImageGallery() {
             height={300}
             className="h-full w-full object-cover"
           ></Image>
-          <div className="shadow-gallery absolute bottom-0 left-0 flex w-full flex-col justify-center gap-2 rounded-lg bg-light-secondary p-4 text-start opacity-0 transition-opacity duration-500 ease-in-out focus:opacity-100 group-hover:opacity-100 dark:bg-dark-secondary">
+          <div className="absolute bottom-0 left-0 flex w-full flex-col justify-center gap-2 rounded-lg bg-light-secondary p-4 text-start opacity-0 shadow-gallery transition-opacity duration-500 ease-in-out focus:opacity-100 group-hover:opacity-100 dark:bg-dark-secondary">
             <p className="dark:text-li font-ubuntu text-xl font-medium leading-[22px] text-light-head dark:text-dark-head">
               {item.title}
             </p>
