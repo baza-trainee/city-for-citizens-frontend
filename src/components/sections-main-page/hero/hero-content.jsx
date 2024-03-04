114788beba3496396527a6e047750819f1dc1b44
Bohdana Lutska <87516048+bohdanalu@users.noreply.github.com>
2024-03-04 08:41:49 +0200

    Fixbug/288,289/add different images and titles for slides (#126)
    * feat: create section Hero-slider
    
    * fix: add images and titles to slides
    
    * Updated the title on one of the slides

--- src/components/sections-main-page/hero/hero-content.jsx
+++ src/components/sections-main-page/hero/hero-content.jsx
@@ -3,26 +3,29 @@ import { useTranslations } from 'next-intl';
 import { Link as ScrollLink } from 'react-scroll';
 import ButtonMainPage from '@/components/common/button-main-page';
 
-export default function HeroContent() {
+export default function HeroContent({ title, subtitle }) {
   const t = useTranslations('Hero');
 
+  const hasUkrainianRoot = text => {
+    const regex = /(укр|ukr)/i;
+    return regex.test(text);
+  };
+
   return (
-    <div className="absolute	 start-1/2 top-[76px]	z-10 w-full -translate-x-1/2 tablet:top-[191px]  tablet:w-[660px]">
+    <div className="absolute start-1/2	 top-[76px] z-10	w-full -translate-x-1/2 tablet:top-[191px]  tablet:w-[660px]">
       <div className="text-center text-white max-tablet:container">
         <h1>
-          {t('title')
-            .split(' ')
-            .map((word, index) => (
-              <span
-                key={index}
-                className={`font-ubuntu text-[40px] font-bold leading-[1.1] tablet:text-[57px] ${index === 2 ? 'text-dark-accent' : ''}`}
-              >
-                {word} {index !== 0 && ' '}
-              </span>
-            ))}
+          {title.split(' ').map((word, index) => (
+            <span
+              key={index}
+              className={`font-ubuntu text-[40px] font-bold leading-[1.1] tablet:text-[57px] ${hasUkrainianRoot(word) ? 'text-dark-accent' : ''}`}
+            >
+              {word} {index !== 0 && ' '}
+            </span>
+          ))}
         </h1>
         <p className="mx-auto mb-8 mt-3 font-roboto leading-[1.4] tablet:w-[475px]">
-          {t('subtitle')}
+          {subtitle}
         </p>
         <ScrollLink
           to="gallery"
