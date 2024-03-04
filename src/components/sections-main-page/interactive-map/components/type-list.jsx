2367c3538291c0ff6bf797bbc987cbf270f131ab
vasylkivt <vasylkiv.t@gmail.com>
2024-03-04 09:10:00 +0200

    fix: event type list

--- src/components/sections-main-page/interactive-map/components/type-list.jsx
+++ src/components/sections-main-page/interactive-map/components/type-list.jsx
@@ -2,7 +2,7 @@ import clsx from 'clsx';
 
 export function TypeList({ eventTypes, className }) {
   return (
-    <ul className={`${clsx('flex gap-2', className)}`}>
+    <ul className={`${clsx('flex flex-wrap gap-2', className)}`}>
       {eventTypes.map(({ id, idIdentifier, eventType }) => (
         <li
           className="rounded-lg border border-light-border px-2 py-1 dark:border-dark-border"
