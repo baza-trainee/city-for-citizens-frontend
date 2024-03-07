93e8aeb435214c3f350c554b260bc16755b7f017
Kateryna Verveda <katushka211@gmail.com>
2024-03-08 00:53:01 +0100

    add color for backdrop

--- tailwind.config.js
+++ tailwind.config.js
@@ -86,6 +86,7 @@ module.exports = {
         primary: '#FFFFFF',
         secondary: '#F2F2F2',
         border: '#AEAEAE',
+        backdrop: '#00000080',
 
         button: {
           hover: '#464646',
