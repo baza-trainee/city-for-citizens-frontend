e87176d60a8d661c4d3bb3edda0c01a4e7335d9f
Kateryna Verveda <113642729+katushka211@users.noreply.github.com>
2024-03-08 21:11:39 +0100

    Feature/#306/create modal for gallery (#136)
    * add translation for gallery-title
    
    * update styles for arrows
    
    * add color for backdrop
    
    * add icon close
    
    * add modal for slider
    
    * add modal component for slider
    
    * add cursor pointer for slides
    
    * update slider
    
    * update z-index for backdrop

--- src/components/sections-main-page/gallery/slider.jsx
+++ src/components/sections-main-page/gallery/slider.jsx
@@ -14,6 +14,8 @@ import data from '../common/data.json';
 
 import { Grid, Pagination, Navigation } from 'swiper/modules';
 import Image from 'next/image';
+import { useEffect, useState } from 'react';
+import { ModalGallery } from './modal';
 
 export function ImageGallery() {
   function getWidthByIndex(index) {
@@ -26,74 +28,119 @@ export function ImageGallery() {
     }
   }
 
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [windowWidth, setWindowWidth] = useState(null);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+    handleResize();
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  function handleSlideClick(image) {
+    setSelectedImage(image);
+    if (windowWidth >= 768) {
+      setIsModalOpen(true);
+    }
+
+    document.body.classList.add('overflow-hidden');
+  }
+
+  function handleModalClose() {
+    setSelectedImage(null);
+    setIsModalOpen(false);
+    document.body.classList.remove('overflow-hidden');
+  }
+
   return (
-    <Swiper
-      grid={{
-        fill: 'row',
-      }}
-      slidesPerView={'auto'}
-      spaceBetween={16}
-      pagination={{
-        clickable: true,
-      }}
-      mousewheel={true}
-      keyboard={true}
-      modules={[Grid, Pagination, Navigation]}
-      breakpoints={{
-        768: {
-          navigation: {
-            prevEl: '.gallery-swiper-swiper-button-prev',
-            nextEl: '.gallery-swiper-swiper-button-next',
+    <>
+      <Swiper
+        grid={{
+          fill: 'row',
+        }}
+        slidesPerView={'auto'}
+        spaceBetween={16}
+        pagination={{
+          clickable: true,
+        }}
+        mousewheel={true}
+        keyboard={true}
+        modules={[Grid, Pagination, Navigation]}
+        breakpoints={{
+          768: {
+            navigation: {
+              prevEl: '.gallery-swiper-swiper-button-prev',
+              nextEl: '.gallery-swiper-swiper-button-next',
+            },
+            grid: {
+              rows: 2,
+            },
           },
-          grid: {
-            rows: 2,
+          960: {
+            spaceBetween: 30,
+            navigation: {
+              prevEl: '.gallery-swiper-swiper-button-prev',
+              nextEl: '.gallery-swiper-swiper-button-next',
+            },
+            grid: {
+              rows: 2,
+            },
           },
-        },
-        960: {
-          spaceBetween: 30,
-          navigation: {
-            prevEl: '.gallery-swiper-swiper-button-prev',
-            nextEl: '.gallery-swiper-swiper-button-next',
-          },
-          grid: {
-            rows: 2,
-          },
-        },
-      }}
-      className="gallery-swiper relative"
-    >
-      <div className="gallery-swiper-swiper-button-prev dark:bg-dark-butt absolute  bottom-[53%] z-10 hidden h-12 w-12 rounded-[40px] bg-light-button-default text-center opacity-50 dark:bg-dark-button-default tablet:left-4 tablet:block laptop:left-10 desktop:left-[10px]">
-        <ArrowLeftIcon className="absolute left-[40%] top-[40%] h-6 w-6 -translate-x-1/2 -translate-y-1/2 transform" />
-      </div>
-      <div className="gallery-swiper-swiper-button-next absolute bottom-[53%] z-10 h-12 w-12 rounded-[40px] bg-light-button-default opacity-50 dark:bg-dark-button-default mobile:hidden  tablet:right-4 tablet:block laptop:right-10 desktop:right-[10px]">
-        <ArrowRightIcon className="absolute left-[40%] top-[40%] h-6 w-6 -translate-x-1/2 -translate-y-1/2 transform" />
-      </div>
-      {data?.map((item, index) => (
-        <SwiperSlide
-          key={index}
-          style={{ width: getWidthByIndex(index), height: '300px' }}
-          className="gallery-swiper group relative overflow-hidden rounded-lg"
-        >
-          <Image
-            src={item.url}
-            alt={item.title}
-            width={400}
-            height={300}
-            className="h-full w-full object-cover"
-          ></Image>
-          <div className="absolute bottom-0 left-0 flex w-full flex-col justify-center gap-2 rounded-lg bg-light-secondary p-4 text-start opacity-0 shadow-gallery transition-opacity duration-500 ease-in-out focus:opacity-100 group-hover:opacity-100 dark:bg-dark-secondary">
-            <p className="dark:text-li font-ubuntu text-xl font-medium leading-[22px] text-light-head dark:text-dark-head">
-              {item.title}
-            </p>
-            <div className="flex items-center  gap-2 text-start">
-              <IconLocation width="24px" height="24px" />
-              <p className="font-roboto text-sm font-normal leading-[19.6px] text-light-head dark:text-dark-head">
-                {item.address}
+        }}
+        className="gallery-swiper relative"
+      >
+        <div className=" gallery-swiper-swiper-button-prev absolute bottom-[53%]  z-10 hidden h-12 w-12 rounded-[40px] bg-light-button-default text-center opacity-50 hover:bg-light-button-hover hover:opacity-100 active:bg-light-button-pressed dark:bg-dark-button-default dark:hover:bg-dark-button-hover dark:hover:opacity-100 dark:active:bg-dark-button-pressed tablet:left-4 tablet:block laptop:left-10 desktop:left-[10px]">
+          <ArrowLeftIcon className="absolute left-[40%] top-[40%] h-6 w-6 -translate-x-1/2 -translate-y-1/2 transform" />
+        </div>
+        <div className="gallery-swiper-swiper-button-next  absolute bottom-[53%] z-10 h-12 w-12 rounded-[40px] bg-light-button-default opacity-50 hover:bg-light-button-hover hover:opacity-100 active:bg-light-button-pressed dark:bg-dark-button-default dark:hover:bg-dark-button-hover dark:hover:opacity-100 dark:active:bg-dark-button-pressed mobile:hidden  tablet:right-4 tablet:block laptop:right-10 desktop:right-[10px] ">
+          <ArrowRightIcon className="absolute left-[40%] top-[40%] h-6 w-6 -translate-x-1/2 -translate-y-1/2 transform" />
+        </div>
+
+        {data?.map((item, index) => (
+          <SwiperSlide
+            key={index}
+            onClick={() => handleSlideClick(item)}
+            style={{ width: getWidthByIndex(index), height: '300px' }}
+            className="gallery-swiper group relative transform cursor-pointer             overflow-hidden
+            rounded-lg
+            transition-transform
+            duration-300
+            ease-in-out hover:scale-105"
+          >
+            <Image
+              src={item.url}
+              alt={item.title}
+              width={400}
+              height={300}
+              className="h-full w-full object-cover"
+            ></Image>
+            <div className="absolute bottom-0 left-0 flex w-full flex-col justify-center gap-2 rounded-[5px] bg-light-secondary p-4 text-start opacity-0 shadow-gallery transition-opacity duration-500 ease-in-out focus:opacity-100 group-hover:opacity-100 dark:bg-dark-secondary">
+              <p className="dark:text-li font-ubuntu text-xl font-medium leading-[22px] text-light-head dark:text-dark-head">
+                {item.title}
               </p>
+              <div className="flex items-center  gap-2 text-start">
+                <IconLocation width="24px" height="24px" />
+                <p className="font-roboto text-sm font-normal leading-[19.6px] text-light-head dark:text-dark-head">
+                  {item.address}
+                </p>
+              </div>
             </div>
-          </div>
-        </SwiperSlide>
-      ))}
-    </Swiper>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+      {isModalOpen && selectedImage && (
+        <ModalGallery
+          selectedImage={selectedImage}
+          modalClose={handleModalClose}
+        />
+      )}
+    </>
   );
 }
