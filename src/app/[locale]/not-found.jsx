3d3d711643f8064b6bdb3f3221bbfb38829e3f89
Volodymyr Peretiatko <42965749+djbob2000@users.noreply.github.com>
2024-03-18 23:02:13 +0200

    feat: create not found page (#151)
    * feat: create not found page
    
    * fix size photos

--- src/app/[locale]/not-found.jsx
+++ src/app/[locale]/not-found.jsx
@@ -1,5 +1,31 @@
-import { Link } from '@/navigation';
+'use client';
+
+import { useTranslations } from 'next-intl';
 
 export default function NotFoundPage() {
-  return <Link href="/">BACK</Link>;
+  const t = useTranslations('NotFound');
+  return (
+    <>
+      <div className="grid h-screen w-full place-content-center bg-light-primary dark:bg-dark-primary">
+        <div className="flex flex-col items-center justify-center">
+          <h1 className="mb-3 text-center font-ubuntu text-[57px] font-bold leading-[62.70px] text-light-head dark:text-dark-head">
+            404
+          </h1>
+          <p className="mb-[32px] text-center font-roboto text-base font-normal leading-snug text-light-head dark:text-dark-head">
+            {t('title')}
+          </p>
+
+          <a
+            href="/"
+            className="inline-flex min-h-[43px] min-w-[240px] cursor-pointer items-center justify-center rounded-md 
+  bg-light-button-default font-roboto text-base font-medium  leading-[1.2] text-light-button-text transition-all 
+   hover:bg-light-button-hover active:bg-light-button-pressed disabled:cursor-not-allowed  dark:bg-dark-button-default
+    dark:text-dark-button-text dark:hover:bg-dark-button-hover dark:active:bg-dark-button-pressed"
+          >
+            {t('button')}
+          </a>
+        </div>
+      </div>
+    </>
+  );
 }
--- src/components/sections-main-page/partners/partners-title.jsx
+++ src/components/sections-main-page/partners/partners-title.jsx
@@ -5,7 +5,7 @@ import { useTranslations } from 'next-intl';
 export default function PartnersTitle() {
   const t = useTranslations('Partners');
   return (
-    <h2 className="mb-[32px] font-ubuntu text-[33px] font-bold leading-[47.30px] text-light-head dark:text-dark-accent tablet:text-[43px]">
+    <h2 className="mb-[32px] font-ubuntu text-[30px] font-bold leading-[47.30px] text-light-head dark:text-dark-accent tablet:text-[43px]">
       {t('title')}
     </h2>
   );
