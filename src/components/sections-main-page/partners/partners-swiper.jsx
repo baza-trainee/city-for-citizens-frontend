668b5fc26cf35fef989852e7721d2a0adc4bf85e
Volodymyr Peretiatko <djbob2000@gmail.com>
2024-03-18 22:25:25 +0200

    fix size photos

--- src/components/sections-main-page/partners/partners-swiper.jsx
+++ src/components/sections-main-page/partners/partners-swiper.jsx
@@ -4,20 +4,19 @@ import { Autoplay } from 'swiper/modules';
 import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
-import { useMedia } from 'react-use';
 
 export default function PartnersSwiper({ partners }) {
-  const isTablet = useMedia('(min-width: 768px)');
-  const spaceBetween = isTablet ? 120 : 60;
-  const imageHeight = isTablet ? '100' : '62';
-  const imageWidth = isTablet ? '160' : '100';
-
   return (
     <div className="py-[20px]">
       <Swiper
         className="marquee"
         modules={[Autoplay]}
-        spaceBetween={spaceBetween}
+        spaceBetween={60}
+        breakpoints={{
+          768: {
+            spaceBetween: 120,
+          },
+        }}
         slidesPerView={'auto'}
         centeredSlides={true}
         autoplay={{
@@ -41,14 +40,13 @@ export default function PartnersSwiper({ partners }) {
                 href={partner.link}
                 target="_blank"
                 rel="noopener noreferrer"
-                className={`relative flex h-[${imageHeight}px] w-[${imageWidth}px] items-center justify-center overflow-hidden`}
+                className={`relative flex h-[62px] w-[100px] items-center justify-center overflow-hidden tablet:h-[100px] tablet:w-[160px]`}
               >
                 <Image
                   src={`${process.env.NEXT_PUBLIC_PARTNERS_IMAGE_BASE_URL}/${partner.image}`}
                   alt={partner.name}
-                  width={imageWidth}
-                  height={imageHeight}
-                  className="object-cover"
+                  fill
+                  className="h-full w-auto object-cover"
                 />
               </Link>
             </SwiperSlide>
